741e308f3b07cb3f6045d9fd67eb0aff35f39e35
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-09-27 19:07:42 -0300

    [Feature] reform layout card for undraw component

--- src/components/UndrawComponent.jsx
+++ src/components/UndrawComponent.jsx
@@ -1,29 +1,38 @@
-import { Heading, Image, Stack, Text } from "@chakra-ui/react";
+import { Box, Heading, Image, Stack, Text } from "@chakra-ui/react";
 import undrawBG from "../assets/UndrawBG.svg";
 import React from "react";
 
 const UndrawComponent = ({ data: { imgUrl, title, paragraph, imgAlt } }) => {
   return (
-    <Stack textAlign="center" alignItems="center" w="xs" p="2">
-      <Stack
-        h="fit-content"
-        justify="center"
-        boxSize="18rem"
-        backgroundImage={`url(${undrawBG})`}
-        backgroundPosition="center"
-        backgroundRepeat="no-repeat"
-      >
-        <Image w="288px" src={imgUrl} alt={imgAlt} />
+    <Stack
+      textAlign="center"
+      alignItems={{ base: "flex-start", sm: "center" }}
+      w={{ base: "full", sm: "xs" }}
+      p="2"
+      direction={{ base: "row", sm: "column" }}
+      bg="#d8d8d80f"
+    >
+      <Stack alignItems="center" minW="135px">
+        <Stack
+          h="fit-content"
+          justify="center"
+          boxSize={{ base: "8rem", sm: "18rem" }}
+          backgroundImage={`url(${undrawBG})`}
+          backgroundPosition="center"
+          backgroundRepeat="no-repeat"
+        >
+          <Image w="288px" src={imgUrl} alt={imgAlt} />
+        </Stack>
+        <Heading
+          color="#000"
+          textTransform="uppercase"
+          fontSize={{ base: "lg", sm: "2xl" }}
+          letterSpacing={3}
+          fontWeight="500"
+        >
+          {title}
+        </Heading>
       </Stack>
-      <Heading
-        color="#000"
-        textTransform="uppercase"
-        fontSize="2xl"
-        letterSpacing={3}
-        fontWeight="500"
-      >
-        {title}
-      </Heading>
       <Text fontSize="md" color="deepGray">
         {paragraph}
       </Text>
