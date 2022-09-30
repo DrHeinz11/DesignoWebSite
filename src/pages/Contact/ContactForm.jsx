4fac7169d1f6e36389cc992ebfb318e578e50812
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-09-30 15:13:15 -0300

    [Feat]Add Contact form handler

--- src/pages/Contact/ContactForm.jsx
+++ src/pages/Contact/ContactForm.jsx
@@ -1,11 +1,24 @@
 import { chakra, Stack, Button, Text, Box, Heading } from "@chakra-ui/react";
-
+import { useState } from "react";
 const ContactForm = () => {
+  const [formData, setFormData] = useState({});
+  const handleSubmit = () => {
+    ev.preventDefault();
+    setFormData(ev.target.data);
+  };
   return (
-    <Stack direction="row" p={10} bg="mainOrange" borderRadius='xl' boxShadow='sm'>
-      <Box flex='1' margin='auto 0' >
-        <Heading color='#340000' p={2}>Contact Us</Heading>
-        <Text color='#000' px={2}>
+    <Stack
+      direction="row"
+      p={10}
+      bg="mainOrange"
+      borderRadius="xl"
+      boxShadow="sm"
+    >
+      <Box flex="1" margin="auto 0">
+        <Heading color="#340000" p={2}>
+          Contact Us
+        </Heading>
+        <Text color="#000" px={2}>
           Ready to take it to the next level? Let’s talk about your project or
           idea and find out how we can help your business grow. If you are
           looking for unique digital experiences that’s relatable to your users,
@@ -13,14 +26,17 @@ const ContactForm = () => {
         </Text>
       </Box>
       <chakra.form
-      flex='1'
+        onSubmit={(ev) => handleSubmit(ev)}
+        flex="1"
         display="flex"
         flexDirection="column"
         gap={10}
         px="10"
         py="5"
       >
-        <chakra.input py={3} color='#fff'
+        <chakra.input
+          py={3}
+          color="#fff"
           bg="transparent"
           type="text"
           required
@@ -29,7 +45,9 @@ const ContactForm = () => {
           _placeholder={{ opacity: 0.5, color: "#fff" }}
           borderBottom="1px solid #fff"
         />
-        <chakra.input py={3} color='#fff'
+        <chakra.input
+          py={3}
+          color="#fff"
           bg="transparent"
           type="text"
           required
@@ -38,7 +56,9 @@ const ContactForm = () => {
           _placeholder={{ opacity: 0.5, color: "#fff" }}
           borderBottom="1px solid #fff"
         />
-        <chakra.input py={3} color='#fff'
+        <chakra.input
+          py={3}
+          color="#fff"
           bg="transparent"
           type="text"
           required
@@ -47,7 +67,9 @@ const ContactForm = () => {
           _placeholder={{ opacity: 0.5, color: "#fff" }}
           borderBottom="1px solid #fff"
         />
-        <chakra.input py={3} color='#fff'
+        <chakra.input
+          py={3}
+          color="#fff"
           bg="transparent"
           type="textarea"
           required
@@ -56,7 +78,13 @@ const ContactForm = () => {
           _placeholder={{ opacity: 0.5, color: "#fff" }}
           borderBottom="1px solid #fff"
         />
-        <Button fontWeight='light' size="md" w='fit-content' alignSelf={'flex-end'} variant="solid">
+        <Button
+          fontWeight="light"
+          size="md"
+          w="fit-content"
+          alignSelf={"flex-end"}
+          variant="solid"
+        >
           Send Message
         </Button>
       </chakra.form>
