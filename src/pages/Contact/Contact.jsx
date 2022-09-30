2215d52dff134dad1e6d8138735ae1b4ac6b7a56
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-09-30 15:10:44 -0300

    [Feat]Add Contact form

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,4 +1,5 @@
 import PagesHeading from "../AboutUs/PagesHeading";
+import ContactForm from "./ContactForm";
 
 const Contact = () => {
   return (
@@ -8,6 +9,7 @@ const Contact = () => {
         copy="Puedes contactarnos via email o por nuestras redes sociales"
         imageUrl="https://images.unsplash.com/photo-1543269865-cbf427effbad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
       />
+      <ContactForm/>
     </>
   );
 };
---
+++ src/pages/Contact/ContactForm.jsx
@@ -0,0 +1,67 @@
+import { chakra, Stack, Button, Text, Box, Heading } from "@chakra-ui/react";
+
+const ContactForm = () => {
+  return (
+    <Stack direction="row" p={10} bg="mainOrange" borderRadius='xl' boxShadow='sm'>
+      <Box flex='1' margin='auto 0' >
+        <Heading color='#340000' p={2}>Contact Us</Heading>
+        <Text color='#000' px={2}>
+          Ready to take it to the next level? Let’s talk about your project or
+          idea and find out how we can help your business grow. If you are
+          looking for unique digital experiences that’s relatable to your users,
+          drop us a line.
+        </Text>
+      </Box>
+      <chakra.form
+      flex='1'
+        display="flex"
+        flexDirection="column"
+        gap={10}
+        px="10"
+        py="5"
+      >
+        <chakra.input py={3} color='#fff'
+          bg="transparent"
+          type="text"
+          required
+          placeholder="Name"
+          _focusVisible={{ outline: 0 }}
+          _placeholder={{ opacity: 0.5, color: "#fff" }}
+          borderBottom="1px solid #fff"
+        />
+        <chakra.input py={3} color='#fff'
+          bg="transparent"
+          type="text"
+          required
+          placeholder="Email Addres"
+          _focusVisible={{ outline: 0 }}
+          _placeholder={{ opacity: 0.5, color: "#fff" }}
+          borderBottom="1px solid #fff"
+        />
+        <chakra.input py={3} color='#fff'
+          bg="transparent"
+          type="text"
+          required
+          placeholder="Phone"
+          _focusVisible={{ outline: 0 }}
+          _placeholder={{ opacity: 0.5, color: "#fff" }}
+          borderBottom="1px solid #fff"
+        />
+        <chakra.input py={3} color='#fff'
+          bg="transparent"
+          type="textarea"
+          required
+          placeholder="Your message"
+          _focusVisible={{ outline: 0 }}
+          _placeholder={{ opacity: 0.5, color: "#fff" }}
+          borderBottom="1px solid #fff"
+        />
+        <Button fontWeight='light' size="md" w='fit-content' alignSelf={'flex-end'} variant="solid">
+          Send Message
+        </Button>
+      </chakra.form>
+    </Stack>
+  );
+};
+
+export default ContactForm;
